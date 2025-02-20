082f0d077c7c2e795b04174db5d5c8dbce30e946
Wallace Santana <wallacesantanak0@gmail.com>
2025-02-20 13:00:44 -0300

    (feat): calculatorWithMethod

---
+++ day_30(calculatorWithMethod)/index.js
@@ -0,0 +1,37 @@
+class Calculator {
+    constructor(result) {
+        this.result = result;
+    }
+
+    add(value) {
+        this.result += value;
+        return this;
+    }
+
+    subtract(value) {
+        this.result -= value;
+        return this;
+    }
+
+    multiply(value) {
+        this.result *= value;
+        return this;
+    }
+
+    divide(value) {
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed");
+        }
+        this.result /= value;
+        return this;
+    }
+
+    power(value) {
+        this.result **= value;
+        return this;
+    }
+
+    getResult() {
+        return Number(this.result.toFixed(5));
+    }
+}
